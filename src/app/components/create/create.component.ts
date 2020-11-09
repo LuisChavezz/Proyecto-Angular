dce60d0568e2c86a19b2cf5d21defe388ae4f6cb
LuisChavezz <ing.luischavezz@gmail.com>
2020-11-08 20:26:15 -0600

    upload image

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../models/project'; // Cargamos el modelo
 import { ProjectService } from '../../services/project.service'; // Cargamos nuestro servicio 'project'
+import { UploadService } from '../../services/upload.service'; // Cargamos nuestro servicio 'upload'
+import { global } from '../../services/global';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService] // Uso de Servicios
+  providers: [ProjectService, UploadService] // Uso de Servicios
 })
 export class CreateComponent implements OnInit {
   public title: string;
   public project: Project; // Guarda el modelo 'project'
   public status: string;
+  public filesToUpload: Array<File>;
 
   constructor(
-    private _projectService: ProjectService //Guarda el servicio
+    private _projectService: ProjectService, //Guarda el servicio
+    private _uploadService: UploadService //Guarda el servicio
   ) { 
       this.title = "Crear Proyecto";
       this.project = new Project('', '', '', '', 2020, '', ''); // Creación del objeto Project
@@ -24,11 +28,19 @@ export class CreateComponent implements OnInit {
   }
 
   onSubmit(form){
+    //Save Info
     this._projectService.saveProjects(this.project).subscribe(
       response => {
         if(response.project){ // Si se recibe el objeto JSON correctamente
-          this.status = 'success';
-          form.reset();
+          
+          //Upload Image
+          this._uploadService.makeFileRequest( global.url+"upload-image/"+response.project._id, [], this.filesToUpload, 'image' )
+          .then((result:any) => {
+            console.log(result);
+            this.status = 'success';
+            form.reset();
+          });
+
         }
         else{
           this.status = 'failed';
@@ -36,6 +48,10 @@ export class CreateComponent implements OnInit {
       },
       error => { console.log(<any>error); }
     );
+  }
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 
 }
