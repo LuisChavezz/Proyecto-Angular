00c0df3e7a3789eaf2aa3d43680f4fe38f590e49
LuisChavezz <ing.luischavezz@gmail.com>
2020-11-08 23:43:20 -0600

    post projects method

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project'; // Cargamos el modelo
+import { ProjectService } from '../../services/project.service'; // Cargamos nuestro servicio 'project'
+import { global } from '../../services/global';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ProjectService] // Uso de Servicios
 })
 export class ProjectsComponent implements OnInit {
+  public projects: Project[];
+  public url: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _projectService: ProjectService
+  ) { 
+    this.url = global.url;
   }
 
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects(){
+    this._projectService.getProjects().subscribe(
+      response => {
+        if(response.projects){
+          this.projects = response.projects;
+        }
+      },
+      error => {
+        console.error(<any>error);
+      }
+    )
+  }
 }
